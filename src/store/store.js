67dc63fe6a988f51653a908c9bbda5fb3a6aee06
Amanda Winkles <amandawinkles@gmail.com>
2020-05-20 21:26:26 -0500

    Complete connecting combine reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -5,10 +5,18 @@ import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
 
+const store = createStore(
+    rootReducer,
+    exampleVideoData,
+    applyMiddleware(thunk)
+  );
 
+/*
+export default const store =
+  return createStore()
+*/
 
-
-
+export default store;
 
 
 
